fe3f09484ca806c323b2876ca8d70aee236e68fa
labibahmed10 <labib.ahmed.372@gmail.com>
2022-03-23 16:03:25 +0600

    cart side added also styles

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -8,21 +8,23 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Cart = () => {
   return (
-    <section>
-      <h1>Order Summary</h1>
-      <p>Selected Item : </p>
-      <p>Total Price : </p>
-      <p>Total Shipping Price : </p>
-      <p>Tax : </p>
-      <h2>Grand Total : </h2>
+    <section className="main-cart">
+      <div className="info-container">
+        <h1>Order Summary</h1>
+        <p>Selected Item : </p>
+        <p>Total Price : </p>
+        <p>Total Shipping Price : </p>
+        <p>Tax : </p>
+        <h2>Grand Total : </h2>
 
-      <button className="btn-1">
-        Clear Cart <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
-      </button>
+        <button className="btn-1">
+          Clear Cart <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
+        </button>
 
-      <button className="btn-2">
-        Review Order <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-      </button>
+        <button className="btn-2">
+          Review Order <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+        </button>
+      </div>
     </section>
   );
 };
