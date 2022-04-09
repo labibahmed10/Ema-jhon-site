243405a26943f06a80d70f176ea1f40677df5424
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-09 23:48:25 +0600

    preparing login and sign in form

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -4,32 +4,55 @@ import { Link } from "react-router-dom";
 const LogIn = () => {
   return (
     <div>
-      <div className="w-96 mx-auto px-5 py-3">
-        <h1 className="text-center text-4xl font-semibold mb-4">Sign Up</h1>
+      <div className="w-96 mx-auto px-5 py-3 text-[#2A414F]">
+        <h1 className="text-center text-4xl font-semibold mb-4 text-[#2A414F]">Sign Up</h1>
         <div className="my-4">
           <label className="text-xl font-semibold" htmlFor="email">
             Email
           </label>
           <br />
-          <input className="border w-full" type="email" name="email" />
+          <input
+            className="border border-[#95A0A7] rounded-md w-full py-3 px-3"
+            type="email"
+            name="email"
+            placeholder="Email here"
+          />
         </div>
         <div className="my-4">
           <label className="text-xl font-semibold" htmlFor="password">
             Password
           </label>
           <br />
-          <input className="border w-full" type="password" name="password" />
+          <input
+            className="border border-[#95A0A7] rounded-md w-full py-3 px-3"
+            type="password"
+            name="password"
+            placeholder="Password here"
+          />
         </div>
         <div className="my-4">
           <label className="text-xl font-semibold" htmlFor="confirmpassword">
             Confirm Password
           </label>
           <br />
-          <input className="border w-full" type="password" name="confirmpassword" />
+          <input
+            className="border border-[#95A0A7] rounded-md w-full py-3 px-3"
+            type="password"
+            name="confirmpassword"
+            placeholder="Confrim Password"
+          />
         </div>
-        <button>Sign Up</button>
-        <p>
-          Already have an account? <Link to=".">Login</Link>
+        <input
+          className="text-center mx-auto py-3 w-full mb-1 mt-3 bg-[#ff990073] rounded-md"
+          type="submit"
+          value="
+        Sign Up"
+        />
+        <p className="text-center text-sm">
+          Already have an account?{" "}
+          <Link className="text-[#ff9900]" to="/">
+            Login
+          </Link>
         </p>
       </div>
     </div>
